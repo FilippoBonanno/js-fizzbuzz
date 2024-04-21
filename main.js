f200568530a3840f8415722bcec0f2344eff042b
FilippoBonanno <fbonanno15@gmail.com>
2024-04-21 16:13:55 +0200

    ciclo for con fizz e fizz buzz

---
+++ main.js
@@ -0,0 +1,22 @@
+
+for (let i = 1; i <= 100; i++) {
+    //stringa vuota per memorizzare l'output
+    let output = ""; 
+  
+    if (i % 3 === 0) {
+      // Aggiungi "Fizz" all'output se il numero è un multiplo di 3  
+      output += "Fizz"; 
+    }
+  
+    if (i % 5 === 0) {
+        // Aggiungi "Buzz" all'output se il numero è un multiplo di 5
+      output += "Buzz"; 
+    }
+  
+    if (output === "") {
+         // Se la stringa di output è vuota mi stampi il numero i
+      output = i;
+    }
+  
+    console.log(output);
+  }
