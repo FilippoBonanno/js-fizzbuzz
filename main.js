83b871200967b717160ac6e276ca0e3a315a2a51
FilippoBonanno <fbonanno15@gmail.com>
2024-04-22 11:21:12 +0200

    print html

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
+let item = document.querySelector(`ul`);
 
 for (let i = 1; i <= 100; i++) {
-    //stringa vuota per memorizzare l'output
-    let output = ""; 
-  
-    if (i % 3 === 0) {
-      // Aggiungi "Fizz" all'output se il numero è un multiplo di 3  
-      output += "Fizz"; 
-    }
-  
-    if (i % 5 === 0) {
-        // Aggiungi "Buzz" all'output se il numero è un multiplo di 5
-      output += "Buzz"; 
-    }
-  
-    if (output === "") {
-         // Se la stringa di output è vuota mi stampi il numero i
-      output = i;
-    }
-  
-    console.log(output);
-  }
+	//stringa vuota per memorizzare l'output
+	let output = "";
+
+
+	if (i % 3 === 0) {
+		let valore;
+		// Aggiungi "Fizz" all'output se il numero è un multiplo di 3  
+		output += "Fizz";
+		
+
+
+	}
+
+	if (i % 5 === 0) {
+		// Aggiungi "Buzz" all'output se il numero è un multiplo di 5
+		output += "Buzz";
+		
+
+
+	}
+
+	if (output === "") {
+		// Se la stringa di output è vuota mi stampi il numero i
+		output = i;
+	 
+	}
+	console.log(output);
+	item.innerHTML += `<li>${output}</li>`;
+
+}
